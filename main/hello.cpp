f9f40f2df3c600d885ded6da25add6dd07efa3bb
onlyAbhishek <112745877+onlyabhishek@users.noreply.github.com>
2024-03-06 23:51:19 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout<<"Webhook updated";
-  cout << "I have successfully built and run\n";\
+  cout << "I have successfully built and run\n";
+  cout << "Webook Sucessfully enabled";
   return 0;
 }
